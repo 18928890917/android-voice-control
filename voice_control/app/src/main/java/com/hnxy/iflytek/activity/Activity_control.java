63c60f7f27d2aeab27d3bed55a41baa945a01743
Wayne Q <316579098@qq.com>
2018-05-25 20:17:06 +0800

    新建讯飞资源文件夹，创建依赖文件，service文件夹，activity文件夹，移植语音唤醒依赖文件

---
+++ voice_control/app/src/main/java/com/hnxy/iflytek/activity/Activity_control.java
@@ -0,0 +1,9 @@
+package com.hnxy.iflytek.activity;
+
+/**
+ * @author Wayne on 2018/5/25.
+ * @modifier Wayne 2018/5/25 19:48.
+ * @description
+ */
+public class Activity_control {
+}
---
+++ voice_control/app/src/main/java/com/hnxy/iflytek/service/service_control.java
@@ -0,0 +1,9 @@
+package com.hnxy.iflytek.service;
+
+/**
+ * @author Wayne on 2018/5/25.
+ * @modifier Wayne 2018/5/25 19:48.
+ * @description
+ */
+public class service_control {
+}
